0ae13a00dd64f44d4153de8b2948e72dc901adc3
(Student) - Anass Doublal <C00290201@itcarlow.ie>
2022-12-14 11:52:50 +0000

    racing

--- 16_SFML_Games/CarTests/car_controller_test.cpp
+++ 16_SFML_Games/CarTests/car_controller_test.cpp
@@ -55,7 +55,7 @@ TEST(CarController, rotateCar) {
 		car.angle += turnSpeed * car.speed;
 		car.move();
 
-		if (car.angle >= 3.14)
+		if (car.angle >= expectedAngle)
 		{
 			break;
 		}
@@ -64,3 +64,41 @@ TEST(CarController, rotateCar) {
 	EXPECT_TRUE(car.angle >= expectedAngle);
 }
 
+TEST(CarController, findTarget)
+{
+	Car ai;
+
+	ai.x = 0;
+	ai.y = 0;
+
+	const int num = 8; //checkpoints
+
+	bool reachedTarget = false;
+
+	int points[num][2] = { 300, 610,
+						  1270,430,
+						  1380,2380,
+						  1900,2460,
+						  1970,1700,
+						  2550,1680,
+						  2560,3150,
+						  500, 3300 };
+
+	for (double i = 0; i < std::numeric_limits<double>::max(); i++)
+	{
+		ai.move();
+
+		ai.findTarget();
+
+		float tx = points[0][0];
+		float ty = points[0][1];
+
+		if ((ai.x - tx) * (ai.x - tx) + (ai.y - ty) * (ai.y - ty) < 25 * 25)
+		{
+			reachedTarget = true;
+			break;
+		}
+	}
+
+	EXPECT_TRUE(reachedTarget);
+}
