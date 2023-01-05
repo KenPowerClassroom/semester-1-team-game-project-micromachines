31e53c92f00b5cd9bef0edeb76968ef792f388a9
Joshua Boyce Hyland <C00270917@itcarlow.ie>
2023-01-05 18:42:58 +0000

    Fixing tests tryed fixing some of the tests gtests not working, so just a rough idea of how they should work for now

--- 16_SFML_Games/CarTests/car_controller_test.cpp
+++ 16_SFML_Games/CarTests/car_controller_test.cpp
@@ -1,104 +1,105 @@
 #include "pch.h"
-#include "../Car.h"
+#include "../carController.h"
 
-TEST(CarController, moveCarForward) {
-	Car car;
-	
-	car.x = 1; 
-	car.y = 1;
+TEST(CarController, moveCarForward) 
+{
+	carController car;
 
-	car.speed = 1;
-	car.angle = 0;
-	
-	car.move();
-	
-	EXPECT_EQ(1, car.x);
-	EXPECT_EQ(0, car.y);
+	car.setPosition({0, 0});
+	car.setSpeed(1); 
+	car.inputHandler(true, false, false, false); 
+
+	EXPECT_EQ(car.getPosition().y, -1);
 }
 
-TEST(CarController, moveCarBackwards) {
-	Car car;
+TEST(CarController, moveCarBackwards) 
+{
+	carController car;
 
-	car.x = 0;
-	car.y = 1;
+	car.setPosition({ 0, 0 });
+	car.setSpeed(1);
+	car.inputHandler(true, false, false, false);
 
-	car.speed = -1;
-	car.angle = 0;
-
-	car.move();
-
-	EXPECT_EQ(0, car.x);
-	EXPECT_EQ(2, car.y);
+	EXPECT_EQ(car.getPosition().y, -1);
 }
 
-TEST(CarController, rotateCar) {
-	Car car;
+//TEST(CarController, rotateCar) {
+//	Car car;
+//
+//	car.x = 0;
+//	car.y = 0;
+//
+//	car.speed = 0;
+//	car.angle = 0;
+//
+//	float expectedAngle = 3.14;
+//
+//	float turnSpeed = 0.08;
+//	float maxSpeed = 12.0;
+//	float acceleration = 0.2;
+//
+//	for (double i=0; i<std::numeric_limits<double>::max(); i++)
+//	{
+//		if (car.speed < maxSpeed)
+//		{
+//			car.speed += acceleration;
+//		}
+//		car.angle += turnSpeed * car.speed;
+//		car.move();
+//
+//		if (car.angle >= expectedAngle)
+//		{
+//			break;
+//		}
+//	}
+//
+//	EXPECT_TRUE(car.angle >= expectedAngle);
+//}
 
-	car.x = 0;
-	car.y = 0;
-
-	car.speed = 0;
-	car.angle = 0;
-
-	float expectedAngle = 3.14;
-
-	float turnSpeed = 0.08;
-	float maxSpeed = 12.0;
-	float acceleration = 0.2;
-
-	for (double i=0; i<std::numeric_limits<double>::max(); i++)
-	{
-		if (car.speed < maxSpeed)
-		{
-			car.speed += acceleration;
-		}
-		car.angle += turnSpeed * car.speed;
-		car.move();
-
-		if (car.angle >= expectedAngle)
-		{
-			break;
-		}
-	}
-
-	EXPECT_TRUE(car.angle >= expectedAngle);
-}
+//TEST(CarController, rotateCar) {
+//		/*Car car;
+//	
+//		car.x = 0;
+//		car.y = 0;
+//	
+//		car.speed = 0;
+//		car.angle = 0;
+//	
+//		float expectedAngle = 3.14;
+//	
+//		float turnSpeed = 0.08;
+//		float maxSpeed = 12.0;
+//		float acceleration = 0.2;
+//	
+//		for (double i=0; i<std::numeric_limits<double>::max(); i++)
+//		{
+//			if (car.speed < maxSpeed)
+//			{
+//				car.speed += acceleration;
+//			}
+//			car.angle += turnSpeed * car.speed;
+//			car.move();
+//	
+//			if (car.angle >= expectedAngle)
+//			{
+//				break;
+//			}
+//		}
+//	
+//		EXPECT_TRUE(car.angle >= expectedAngle);*/
+//
+//		carController car; 
+//
+//		car.
+//	}
 
 TEST(CarController, findTarget)
 {
-	Car ai;
+	carController car;
 
-	ai.x = 0;
-	ai.y = 0;
+	car.setPosition({ 300, 616 });
+	car.setSpeed(1);
+	car.inputHandler(true, false, false, false); 
 
-	const int num = 8; //checkpoints
-
-	bool reachedTarget = false;
-
-	int points[num][2] = { 300, 610,
-						  1270,430,
-						  1380,2380,
-						  1900,2460,
-						  1970,1700,
-						  2550,1680,
-						  2560,3150,
-						  500, 3300 };
-
-	for (double i = 0; i < std::numeric_limits<double>::max(); i++)
-	{
-		ai.move();
-
-		ai.findTarget();
-
-		float tx = points[0][0];
-		float ty = points[0][1];
-
-		if ((ai.x - tx) * (ai.x - tx) + (ai.y - ty) * (ai.y - ty) < 25 * 25)
-		{
-			reachedTarget = true;
-			break;
-		}
-	}
-
-	EXPECT_TRUE(reachedTarget);
+	EXPECT_TRUE(car.foundTarget({ 300,610 })); 
 }
--- 16_SFML_Games/carController.cpp
+++ 16_SFML_Games/carController.cpp
@@ -63,7 +63,7 @@ void carController::inputHandler(bool Up, bool Down, bool Right, bool Left)
 
  bool carController::foundTarget(sf::Vector2f nextCheckpoint)
 {
-    //m_speed = t_speed; 
+   ; 
     float beta = m_angle - atan2(nextCheckpoint.x -m_position.x, -nextCheckpoint.y + m_position.y);
     m_angle;
     if (sin(beta) < 0)
